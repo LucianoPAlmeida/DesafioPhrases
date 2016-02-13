9bbfbd8036ccd6a32a627d9636fbd62aef1136fe
Luciano Almeida <passos.luciano@outlook.com>
2016-02-13 02:17:22 -0200

    Initial Commit

---
+++ Desafio Phrases/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Desafio Phrases
+//
+//  Created by Luciano Almeida on 2/13/16.
+//  Copyright © 2016 Luciano Almeida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
